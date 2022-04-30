522e0d6bf1873752e32ac85ebf52f4a45e7aff98
gagandeepbajwa[C <gagandeepbajwa1998@gmail.com>
2022-04-30 14:39:27 -0700

    updated predictWord

--- predictWord.py
+++ predictWord.py
@@ -1,11 +1,11 @@
 from autocorrect import spell
 import time
 while 1:
-	f = open('letters.txt','r');
-	text = f.read();
-	print text
-	print spell(text)
-	f.close();
+	f = open('letters.txt','r')
+	text = f.read()
+	print(text)
+	print(spell(text))
+	f.close()
 	f = open('word.txt','w')
 	f.write(spell(text))
 	f.close()
